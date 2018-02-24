8750693ca577fc94d308b3aa78914209df6c7b93
josiahhaswell <josiah.haswell@gmail.com>
2018-02-24 16:51:18 -0700

    Fixing build

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,10 +5,16 @@ source /scripts/set-version.sh
 
 mvn clean install deploy -f bom/pom.xml 
 #mvn versions:set -DnewVersion=${VERSION} -f bom/pom.xml
-gradle clean spotlessApply build pTML publish \
+#gradle clean spotlessApply build pTML publish \
+#-PmavenRepositoryUrl=${MVN_REPO_URL} \
+#-PmavenRepositoryUsername=${MVN_REPO_USERNAME} \
+#-PmavenRepositoryPassword=${MVN_REPO_PASSWORD} \
+#-Pversion=1.0.0-SNAPSHOT \
+#--console=plain -i
+    
+gradle clean build pTML publish \
 -PmavenRepositoryUrl=${MVN_REPO_URL} \
 -PmavenRepositoryUsername=${MVN_REPO_USERNAME} \
 -PmavenRepositoryPassword=${MVN_REPO_PASSWORD} \
 -Pversion=1.0.0-SNAPSHOT \
---console=plain -i
-    
+-Penv.version=1.0.0-SNAPSHOT
