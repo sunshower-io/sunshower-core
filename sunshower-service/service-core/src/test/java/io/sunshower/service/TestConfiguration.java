1a814d978c4d1aaa624e6bab238a262b9b120317
Josiah Haswell <josiah.haswell@gmail.com>
2018-05-04 13:18:56 -0600

    Fixing locations

--- sunshower-service/service-core/src/test/java/io/sunshower/service/TestConfiguration.java
+++ sunshower-service/service-core/src/test/java/io/sunshower/service/TestConfiguration.java
@@ -8,6 +8,7 @@ import io.sunshower.service.security.AuthenticationSession;
 import io.sunshower.service.security.Session;
 import io.sunshower.service.serialization.DynamicJaxrsProviders;
 import io.sunshower.service.serialization.DynamicResolvingMoxyJsonProvider;
+import io.sunshower.test.common.TestClasspath;
 import io.sunshower.test.common.TestConfigurations;
 import io.sunshower.test.persist.ConnectionDetectingJDBCTemplate;
 import java.util.concurrent.ExecutorService;
@@ -35,7 +36,12 @@ public class TestConfiguration {
   @Primary
   @Bean(name = TestConfigurations.TEST_CONFIGURATION_REPOSITORY_PATH)
   public String location() {
-    return "/sunshower-api/core-api/src/test/resources";
+    return TestClasspath.rootDir()
+        .getParent()
+        .getParent()
+        .resolve("sunshower-api/core-api/src/test/resources")
+        .toFile()
+        .getAbsolutePath();
   }
 
   @Bean
