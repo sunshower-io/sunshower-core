934d5350d781f554cef0b9f24657223010865d76
lisagumerman <lisajgumerman@gmail.com>
2018-12-01 18:13:01 -0700

    reformatting

--- sunshower-api/core-api/src/test/java/io/sunshower/model/core/PropertyTest.java
+++ sunshower-api/core-api/src/test/java/io/sunshower/model/core/PropertyTest.java
@@ -1,24 +1,34 @@
 package io.sunshower.model.core;
 
-import org.junit.jupiter.api.Test;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import org.junit.jupiter.api.Test;
+
 class PropertyTest {
 
-    @Test
-    void testAllThePropertiesAtOnce() {
-        for(Property.Type t : Property.Type.values()) {
-            Property p;
-            switch(t) {
-                case Integer: p = Property.integer("schnorp", "porp", "10"); break;
-                case Secret: p = Property.secret("schnorp", "porp", t.toString()); break;
-                case Boolean: p = Property.bool("schnorp", "porp", "true"); break;
-                case Class: p = Property.type("schnorp", "porp", t.toString()); break;
-                default: p = Property.string("schnorp", "porp", t.toString()); break;
-            }
-            assertThat(p.getPropertyType(), is(t));
-        }
+  @Test
+  void testAllThePropertiesAtOnce() {
+    for (Property.Type t : Property.Type.values()) {
+      Property p;
+      switch (t) {
+        case Integer:
+          p = Property.integer("schnorp", "porp", "10");
+          break;
+        case Secret:
+          p = Property.secret("schnorp", "porp", t.toString());
+          break;
+        case Boolean:
+          p = Property.bool("schnorp", "porp", "true");
+          break;
+        case Class:
+          p = Property.type("schnorp", "porp", t.toString());
+          break;
+        default:
+          p = Property.string("schnorp", "porp", t.toString());
+          break;
+      }
+      assertThat(p.getPropertyType(), is(t));
     }
+  }
 }
